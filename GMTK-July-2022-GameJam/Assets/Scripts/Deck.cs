89012f717db974dc3cd64d598f3e527ad7ce49cf
pudgypoultry <50252390+pudgypoultry@users.noreply.github.com>
2022-07-16 20:59:38 -0700

    DeckObject and Script Update

--- GMTK-July-2022-GameJam/Assets/Scripts/Deck.cs
+++ GMTK-July-2022-GameJam/Assets/Scripts/Deck.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Deck : MonoBehaviour
 {
 
-    public List<GameObject> listOfChildren;
+    [SerializeField] public List<GameObject> listOfChildren;
     Transform dieFace;
+    public GameObject specialCase;
 
     public void Start()
     {
         //Test Code, delete later
+        RollTheDie();
         RollTheDie();
         RollTheDie();
         RollTheDie();
@@ -24,8 +26,14 @@ public class Deck : MonoBehaviour
 
         // Get number of child objects, choose a random number from 1 to that number
         int numberOfChildren = listOfChildren.Count;
+
         int randomPick = Random.Range(0, numberOfChildren);
-        Debug.Log("Child picked = " + randomPick);
+
+        if (randomPick == 0)
+        {
+            Debug.Log("We rolled a one: " + specialCase);
+            return specialCase;
+        }
 
         // Set current die face to the random number chosen
         dieFace = listOfChildren[randomPick].transform;
@@ -36,7 +44,6 @@ public class Deck : MonoBehaviour
         // Choose one of those children, then return the associated GameObject
         numberOfChildren = listOfGrandchildren.Count;
         randomPick = Random.Range(0, numberOfChildren);
-        Debug.Log("Grandchild picked = " + randomPick);
         resultingDie = listOfGrandchildren[randomPick];
 
         Debug.Log(resultingDie);
